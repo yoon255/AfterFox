d6440e7a8f74322568ea745d58428c2516d0436e
yoon255 <yjhclass001@gmail.com>
2024-12-08 17:55:37 +0900

    1208Finish

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -6,6 +6,9 @@ using TMPro; // TextMeshPro
 public class GM : MonoBehaviour
 {
     static public GM instance;
+
+    public GameObject can_SNL;
+
     public enum Progress { 퀘스트받기전, 퀘스트받음_수행X, 퀘스트받음_수행O, 퀘스트완료 };
     public Progress eProgress = Progress.퀘스트받기전;
     //TextMeshPro가 아니라 TextMeshProUGUI
@@ -19,6 +22,48 @@ public class GM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            //창이 꺼져있으면 -> 켜주면
+            //켜있으면 -> 까주면 됨.
 
+            //can_SNL.activeSelf: 현재 내 상태를 점검
+            //                    on:  true
+            //                    off: false
+            //! : 반대 
+            //!can_SNL.activeSelf : 내 상태의 반대
+            can_SNL.SetActive(!can_SNL.activeSelf);
+        }
     }
+
+    public void Button_continu()
+    {
+        can_SNL.SetActive(false);
+    }
+    public void Button_save()
+    {
+        //[저장]
+        //퀘스트진행도
+        PlayerPrefs.SetInt("questProcess", (int)eProgress);
+        Debug.Log(PlayerPrefs.GetInt("questProcess"));
+
+        //캐릭터위치
+        //PlayerPrefs: int, string, float 만 저장가능
+        //             따라서, x,y,z를 분리해서 저장.
+        PlayerPrefs.SetFloat("posX", Player.instance.transform.position.x);
+        PlayerPrefs.SetFloat("posY", Player.instance.transform.position.y);
+        PlayerPrefs.SetFloat("posZ", Player.instance.transform.position.z);
+    }
+    public void Button_Load()
+    {
+        //*불러오는것에 그치지 않고, 대입을 해줘야함.
+
+        //저장한 값을 불러옴
+        int process = PlayerPrefs.GetInt("questProcess");
+        //Vector3 (posX, posY, posZ)
+
+        //불러온 값으로 대입
+        eProgress = (Progress)process;
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    static public Player instance;
     public List<string> itemNames;
+    public float speed = 6f;
+    public Rigidbody2D foxRB;
+    public int jumpCount = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1, 0, 0);
+        Move();
+        
+        if(jumpCount < 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpCount++;
+                foxRB.velocity = new Vector2(0, 0);
+                foxRB.AddForce(new Vector2(0, 1) * 20, ForceMode2D.Impulse);
+            }
+        }
     }
+
+    void Move()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(+1, 0, 0) * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            //유니티 회전 
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        //참고: Input.GetAxisRaw("Horizontal"); //-1,0,1
+    }
+
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,6 +74,11 @@ public class Player : MonoBehaviour
                 }
             }
         }
+        if(collision.gameObject.CompareTag("floor"))
+        {
+            jumpCount = 0;
+        }
+
     }
 
 
